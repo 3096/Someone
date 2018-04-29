95fff7c6b8770d6fa7b89a1055fcaf494217eff3
3096 <the3096@gmail.com>
2018-04-29 11:47:33 -0700

    Add LocationHelper class

--- app/src/main/java/com/the3096/someone/LocationHelper.java
+++ app/src/main/java/com/the3096/someone/LocationHelper.java
@@ -1,4 +1,68 @@
 package com.the3096.someone;
 
-public class LocationHelper {
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+
+class LocationHelper {
+    private Activity mContext;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+    private Location mLocation;
+    private String mLocationProvider;
+
+    LocationHelper(Activity context) {
+        mContext = context;
+        mLocationProvider = LocationManager.NETWORK_PROVIDER;
+
+        if (ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mContext,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else {
+            init();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    void init() {
+        mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+
+        final TextView textView = mContext.findViewById(R.id.text);
+        mLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                mLocation = location;
+                textView.setText(mLocation.toString());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        mLocationManager.requestLocationUpdates(mLocationProvider, 0, 0, mLocationListener);
+    }
+
+    Location getLocation() {
+        if(mLocation != null)
+            return mLocation;
+        return new Location(mLocationProvider);
+    }
 }
--- app/src/main/java/com/the3096/someone/MainActivity.java
+++ app/src/main/java/com/the3096/someone/MainActivity.java
@@ -1,62 +1,44 @@
 package com.the3096.someone;
 
-import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Context;
-import android.location.LocationManager;
-import android.location.LocationListener;
-import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationHelper locationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-        }
-/*
-        if (ActivityCompat.checkSelfPermission(YourActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(YourActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(YourActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
-        }else{
-            // Write you code here if permission already given.
-        }*/
+        final TextView textView = findViewById(R.id.text);
 
-        final TextView textView = (TextView) findViewById(R.id.text);
-        try {
-            // Acquire a reference to the system Location Manager
-            LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationHelper = new LocationHelper(this);
 
-            // Define a listener that responds to location updates
-            LocationListener locationListener = new LocationListener() {
-                public void onLocationChanged(Location location) {
-                    // Called when a new location is found by the network location provider.
-                    //makeUseOfNewLocation(location);
+        textView.setText(locationHelper.getLocation().toString());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationHelper.init();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
                 }
-
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-                    textView.setText("");
-                }
-
-                public void onProviderEnabled(String provider) {}
-
-                public void onProviderDisabled(String provider) {}
-            };
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-        } catch (Exception e) {
-            textView.setText("hmm");
-
-        } finally {
-
+            }
         }
     }
 }
